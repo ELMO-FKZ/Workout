23fcf7396168f192f1ab4f3391adc1a0f13021fd
EL MOKHTAR <122396078+ELMO-FKZ@users.noreply.github.com>
2023-09-26 16:54:23 +0200

    Creating the home page component.

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,16 @@
+import Landing from "../../compenents/landing/Landing"
+import Advice from "../../compenents/advice/Advice"
+import Faqs from "../../compenents/faqs/Faqs"
+
+const Home = () => {
+
+    return (
+        <>
+        <Landing />
+        <Advice />
+        <Faqs />
+        </>
+    )
+}
+
+export default Home
